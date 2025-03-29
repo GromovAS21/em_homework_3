89a751f9a1afea2e415588b4a22eefb55d25f7dc
Anton Gromov <seed14112@yandex.ru>
2025-03-29 16:56:29 +0300

    [+] Добавление модели Dogs

--- breeds/models.py
+++ breeds/models.py
@@ -1,3 +1,60 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class SizeChoices(models.TextChoices):
+    """Модель выбора размера породы"""
+    TINY = "Tiny", "Маленький"
+    SMALL = "Small", "Малый"
+    MEDIUM = "Medium", "Средний"
+    LARGE = "Large", "Большой"
+
+
+class Breed(models.Model):
+    """Модель породы"""
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Название породы"
+    )
+    size = models.CharField(
+        max_length=50,
+        choices=SizeChoices
+    )
+    friendliness = models.PositiveSmallIntegerField(
+        verbose_name="Дружелюбие",
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    trainability = models.PositiveSmallIntegerField(
+        verbose_name="Обучаемость",
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    shedding_amount = models.PositiveSmallIntegerField(
+        verbose_name="Объем шерсти",
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+    exercise_needs = models.PositiveSmallIntegerField(
+        verbose_name="Требования к физической активности",
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
+
+    def __str__(self):
+        """Строковое представление объекта класса"""
+        return self.name
+
+    class Meta:
+        verbose_name = "Порода"
+        verbose_name_plural = "Породы"
+        ordering = ["name"]
