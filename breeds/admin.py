9e1314c63588875cc3076f238d71006c728072ae
Anton Gromov <seed14112@yandex.ru>
2025-03-29 16:57:02 +0300

    [+] Добавление админ панели для модели Dogs

--- breeds/admin.py
+++ breeds/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from breeds.models import Breed
+
+
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    """Админка для пород"""
+
+    list_display = ("name", "size", "friendliness", "trainability", "shedding_amount", "exercise_needs")
+    list_filter = ("size", "friendliness", "trainability", "shedding_amount", "exercise_needs")
+    search_fields = ("name", )
