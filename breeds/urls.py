7f14ead77abd5834331d6eedcf4822dddc5caf43
Anton Gromov <seed14112@yandex.ru>
2025-03-29 17:42:34 +0300

    [+] Добавление роутера для модели Breed

--- breeds/urls.py
+++ breeds/urls.py
@@ -1,7 +1,14 @@
+from django.urls import include, path
+from rest_framework import routers
+
 from breeds.apps import BreedsConfig
+from breeds.views import BreedViewSet
 
 app_name = BreedsConfig.name
 
-urlpatterns = [
+router = routers.DefaultRouter()
+router.register(r"",BreedViewSet, basename="breed")
 
+urlpatterns = [
+    path('', include(router.urls)),
 ]
