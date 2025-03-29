03078bf337062439058f55cad305ba639d08356d
Anton Gromov <seed14112@yandex.ru>
2025-03-29 17:36:03 +0300

    [+] Добавление viewsets для модели Breed

--- breeds/views.py
+++ breeds/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from breeds.models import Breed
+from breeds.serializers import BreedSerializer
+
+
+class BreedViewSet(viewsets.ModelViewSet):
+    """ Представление пород собак"""
+
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
