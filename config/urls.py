bb2c37bc31ac8811307765da3c4da7a43d2c635e
Anton Gromov <seed14112@yandex.ru>
2025-03-29 16:57:42 +0300

    [+] Добавление информации для админ-панели

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = 'Администрирование "Собачек и породы"'
+admin.site.site_title = "Собачки и их породы"
+admin.site.index_title = "Администрирование"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/dogs/", include("dogs.urls", namespace="dogs")),
