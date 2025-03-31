6a9db8c800f811e21cfe8da0f9d22a786d026a1b
Anton Gromov <seed14112@yandex.ru>
2025-03-31 09:34:01 +0300

    [+] Добавлен Prefetch в запрос к бд в представлении DogViewSet для вывода среднего возрасто собак каждой породы и общее количество собак с определенной породой

--- dogs/serializers.py
+++ dogs/serializers.py
@@ -9,3 +9,34 @@ class DogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dog
         fields = ("id", "name", "breed", "age", "gender", "color", "favorite_food", "favorite_toy")
+
+
+class DogListSerializer(DogSerializer):
+    """Сериализатор для списка собак"""
+
+    avg_age = serializers.SerializerMethodField()
+
+    class Meta(DogSerializer.Meta):
+        fields = DogSerializer.Meta.fields + ("avg_age",)
+
+    def get_avg_age(self, obj):
+        """Метод для подсчета среднего возраста собак одного вида"""
+        if not hasattr(obj, "avg_age"):
+            return None
+        return obj.avg_age
+
+
+class DogDetailSerializer(DogSerializer):
+    """Сериализатор собаки с детальным выводом"""
+
+    num_same_breed = serializers.SerializerMethodField()
+
+    class Meta(DogSerializer.Meta):
+        fields = DogSerializer.Meta.fields + ("num_same_breed",)
+
+    def get_num_same_breed(self, obj):
+        """Метод для подсчета количества собак одного вида данного экземпляра"""
+
+        if not hasattr(obj, "num_same_breed"):
+            return None
+        return obj.num_same_breed
--- dogs/views.py
+++ dogs/views.py
@@ -1,11 +1,20 @@
+from django.db.models import Avg, Count
 from rest_framework import viewsets
 
 from dogs.models import Dog
-from dogs.serializers import DogSerializer
+from dogs.serializers import DogDetailSerializer, DogListSerializer, DogSerializer
 
 
 class DogViewSet(viewsets.ModelViewSet):
     """Представление собак"""
 
-    queryset = Dog.objects.all()
+    queryset = Dog.objects.annotate(avg_age=Avg("breed__dogs__age"), num_same_breed=Count("breed__dogs"))
     serializer_class = DogSerializer
+
+    def get_serializer_class(self):
+        """Возвращаем сериализаторы в зависимости от действия"""
+        if self.action == "list":
+            return DogListSerializer
+        elif self.action == "retrieve":
+            return DogDetailSerializer
+        return DogSerializer
